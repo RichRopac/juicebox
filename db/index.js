99bc8ddcf627b3eabcacb7bba864c2401d22f37a
rcropac <rcropac@gmail.com>
2022-07-15 11:00:09 -0500

    Second Push

---
+++ db/index.js
@@ -0,0 +1,22 @@
+
+const { Client } = require('pg'); // imports the pg module
+
+
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+
+
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+  }
+
+module.exports = {
+  client,
+  getAllUsers,
+}
