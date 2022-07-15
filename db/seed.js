e70339ffa8aec419bda0f5e1bf4d020e3e9ae99a
Ruby Gonzalez <RubyJ.Gonzalez@outlook.com>
2022-07-15 14:21:15 -0500

    End of Part 1.1

--- db/seed.js
+++ db/seed.js
@@ -9,8 +9,15 @@ async function createInitialUsers() {
     console.log("starting to create users...")
 
     const albert = await createUser({ username: 'albert', password: 'bertie99' });
+    const sandra = await createUser({ username: 'sandra', password: '2sandy4me'});
+    const glamgal = await createUser({ username: 'glamgal', password: 'soglam'})
+   
 
     console.log(albert);
+    console.log(sandra);
+    console.log(glamgal);
+
+    
     console.log('finished creating users!')
   } catch(error) {
     console.error('error creating users');
