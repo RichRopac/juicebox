f2b63b864fe55d8087967970c92ea9468576af74
rcropac <rcropac@gmail.com>
2022-07-21 13:35:01 -0500

    Posts by Tags

--- api/tags.js
+++ api/tags.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
   
     next();
   });
-  
   
   tagsRouter.get('/', async (req, res) => {
     const tags = await getAllTags();
@@ -16,5 +15,20 @@ tagsRouter.use((req, res, next) => {
       "tags": [tags]
     });
   });
+
+  tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    const { tagName } = req.params;
+    try {
+      // use our method to get posts by tag name from the db
+      // send out an object to the client { posts: // the posts }
+      const posts = await getPostsByTagName(tagName)   
+      res.send({ posts: posts })
+    } catch ({ name, message }) {
+      // forward the name and message to the error handler
+      next({ name, message });
+    }
+  });
+
+
 
 module.exports = tagsRouter;
